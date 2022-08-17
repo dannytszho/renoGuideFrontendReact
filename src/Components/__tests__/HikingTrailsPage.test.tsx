a6f86835b01a98e75461db3f1b1c7e24a5b298ac
dannytszho <dannytszho@gmail.com>
2022-08-17 12:21:38 -0500

    Add unit test for testing multiple trails for fetching API

--- src/Components/__tests__/HikingTrailsPage.test.tsx
+++ src/Components/__tests__/HikingTrailsPage.test.tsx
@@ -11,7 +11,7 @@ const trailResponse = rest.get(trailsAPI_URL, (req, res, ctx) => {
     ctx.json([
       {
         id: 1,
-        name: 'Angels Landing',
+        name: 'Angels Landing Trail',
         length: '6.4 mi',
         elevation: '1,240 ft',
         duration: 'Est. 3h 9m',
@@ -21,6 +21,19 @@ const trailResponse = rest.get(trailsAPI_URL, (req, res, ctx) => {
         imageUrl:
           'https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvMTE2OTUzMjcvNTMxNjIwMGIxODJmNjkzMzRkYmJiNTA2YzliNTE4OGQuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==',
         created_at: '2022-07-12T04:14:18.071221Z',
+      },
+      {
+        id: 2,
+        name: 'Galena Creek Trail',
+        length: '4.7 mi',
+        elevation: '774 ft',
+        duration: 'Est. 2h 9m',
+        difficulty: 'Moderate',
+        rating: '4.5',
+        url: 'https://www.alltrails.com/explore/trail/us/nevada/galena-creek-trail',
+        imageUrl:
+          'https://photos.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvMjU0MTI1ODYvYWIyYTczNmY2NzBiM2E0OTUyMWUwNzIzMDFiOTQ3MjcuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==',
+        created_at: '2022-07-12T04:22:17.114791Z',
       },
     ]),
   )
@@ -32,8 +45,14 @@ beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
-test('it should have the correct trails', async () => {
+test('it should have the correct trails, Angels Landing Trail', async () => {
   render(<HikingTrailsPage />, { wrapper: HashRouter })
-  const trail = await screen.findByText('Angels Landing')
+  const trail = await screen.findByText('Angels Landing Trail')
+  expect(trail).toBeVisible()
+})
+
+test('it should have the correct trails, AGalena Creek Trail', async () => {
+  render(<HikingTrailsPage />, { wrapper: HashRouter })
+  const trail = await screen.findByText('Galena Creek Trail')
   expect(trail).toBeVisible()
 })
