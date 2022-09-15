5137cc4cf013c3f3181e79059169a279a6fba103
dannytszho <dannytszho@gmail.com>
2022-09-15 15:12:33 -0500

    Add Home button in Navbar.tsx

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,18 +1,53 @@
-import { Outlet } from 'react-router-dom'
+import { useAuthenticator } from '@aws-amplify/ui-react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import UserIcon from '../svg/UserIcon'
 
 const NavBar = () => {
+  const { route, signOut } = useAuthenticator(context => [
+    context.route,
+    context.signOut,
+  ])
+  const navigate = useNavigate()
+
+  const logOut = () => {
+    signOut()
+    navigate('/')
+  }
   return (
     <>
-      <div className="flex justify-end mt-6 mr-5 font-iceland">
-        <div className="mt-0.5 mr-1">
-          <UserIcon />
-        </div>
-        <h1>User</h1>
-        <button className="w-[68px] h-[30px] ml-4 border rounded-lg font-semibold bg-zinc-200 hover:cursor-pointer hover:scale-[103%]">
-          Log Out
-        </button>
-      </div>
+      <nav>
+        {route !== 'authenticated' ? (
+          <div className="flex justify-end mt-6 mb-28 mr-5 font-iceland">
+            <div>
+              <button
+                className="w-[68px] h-[30px] ml-4 border rounded-lg font-semibold bg-zinc-200 hover:cursor-pointer hover:scale-[103%]"
+                onClick={() => navigate('/')}
+              >
+                Home
+              </button>
+              <button
+                className="w-[68px] h-[30px] ml-4 border rounded-lg font-semibold bg-zinc-200 hover:cursor-pointer hover:scale-[103%]"
+                onClick={() => navigate('/login')}
+              >
+                Log In
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="flex justify-end mt-6 mb-28 mr-5 font-iceland">
+            <div className="mt-0.5 mr-1">
+              <UserIcon />
+            </div>
+            <h1>User</h1>
+            <button
+              className="w-[68px] h-[30px] ml-4 border rounded-lg font-semibold bg-zinc-200 hover:cursor-pointer hover:scale-[103%]"
+              onClick={() => logOut()}
+            >
+              Log Out
+            </button>
+          </div>
+        )}
+      </nav>
       <Outlet />
     </>
   )
