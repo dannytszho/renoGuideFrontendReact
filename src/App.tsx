2b94a891d40274e33d947a6b01b47b5f92d1eab0
dannytszho <dannytszho@gmail.com>
2022-09-15 13:52:34 -0500

    Add and route login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,30 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import HikingTrailsPage from './Components/HikingTrailsPage'
 import HomePage from './Components/HomePage'
-import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react'
+import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import NavBar from './Components/NavBar'
 import HikingTrailsPageGQL from './Components/HikingTrailsPageGQL'
+import Login from './Components/Login'
 
 function App() {
   return (
     <>
-      <div>
-        <header>
-          <HashRouter>
-            <Routes>
-              <Route path="/" element={<NavBar />}>
-                <Route index element={<HomePage />} />
-              </Route>
+      <Authenticator.Provider>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<NavBar />}>
+              <Route index element={<HomePage />} />
               <Route path="/hikingtrails" element={<HikingTrailsPage />} />
               <Route
                 path="/hikingtrailsgql"
                 element={<HikingTrailsPageGQL />}
               />
-            </Routes>
-          </HashRouter>
-        </header>
-      </div>
+              <Route path="/login" element={<Login />} />
+            </Route>
+          </Routes>
+        </HashRouter>
+      </Authenticator.Provider>
     </>
   )
 }
