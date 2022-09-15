fe41f4521c301468bffbab6d89b9036a1541f7bd
dannytszho <dannytszho@gmail.com>
2022-09-15 14:44:11 -0500

    Redirect users to home page if route is authenticated

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,6 +1,31 @@
-import { Authenticator } from '@aws-amplify/ui-react'
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
+import { useEffect, useMemo } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+
 const Login = () => {
-  return <Authenticator></Authenticator>
+  const { route } = useAuthenticator(context => [context.route])
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const navigatePathName = useMemo(() => {
+    const state = location.state as { from: string }
+
+    if (state && state.from) {
+      return state.from
+    }
+    return '/'
+  }, [location])
+
+  useEffect(() => {
+    if (route === 'authenticated') {
+      navigate(navigatePathName, { replace: true })
+    }
+  }, [route, navigate, navigatePathName])
+  return (
+    <div className="auth-wrapper">
+      <Authenticator></Authenticator>
+    </div>
+  )
 }
 export default Login
