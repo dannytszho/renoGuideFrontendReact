ae1fcf88b610fedd47c747824b2b38f8e2db111a
dannytszho <dannytszho@gmail.com>
2022-10-13 00:33:58 -0500

    Add modal

--- src/Components/Utils/TrailsCard.tsx
+++ src/Components/Utils/TrailsCard.tsx
@@ -1,5 +1,6 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { AiOutlineClockCircle } from 'react-icons/ai'
+import Modal from 'react-modal'
 
 export interface CardProps {
   name: string
@@ -29,9 +30,15 @@ const TrailsCard = ({
   rating,
   urL,
 }: CardProps) => {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <div className="bg-white font-iceland text-black m-10 rounded-xl overflow-hidden shadow-lg w-[300px] hover:scale-105 hover:tansition hover:duration-200">
-      <a href={urL} target="_blank" rel="noreferrer">
+    <div>
+      <button
+        onClick={() => setShowModal(true)}
+        className="bg-white font-iceland text-black m-10 rounded-xl overflow-hidden shadow-lg w-[300px] hover:scale-105 hover:tansition hover:duration-200"
+      >
+        {/* <a href={urL} target="_blank" rel="noreferrer"> */}
         <img src={image} alt="placeholder" width="300px" height="168px" />
         <div className="flex justify-between mx-3 mt-2">
           <span
@@ -53,7 +60,56 @@ const TrailsCard = ({
           </span>
           <span>{rating}</span>
         </div>
-      </a>
+        {/* </a> */}
+      </button>
+
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    contents
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   )
 }
